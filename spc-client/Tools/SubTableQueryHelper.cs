e09e2a6d494c5eb2a7b7118e6bfd2ee53d34b814
baymin <yiningzeng@outlook.com>
2021-01-20 20:18:02 +0800

    feat: 支持多种分表查询返回结果

--- spc-client/Tools/SubTableQueryHelper.cs
+++ spc-client/Tools/SubTableQueryHelper.cs
@@ -9,16 +9,22 @@ namespace spc_client.Tools
 {
     public class SubTableQueryHelper<T>
     {
-        public delegate void Callback(List<T> listRes, bool isDone);
-        private int allQueryNum = 0; // 指的是一共查询的月份个数
-        private int currentNum = 0; // 标识当前运行了几个
+        public delegate void Callback(string sql, List<T> listRes, bool isDone);
+
+        public int allQueryNum = 0; //指的是一共查询的月份个数
+        public int currentNum = 0; // 标识当前运行了几个
+
+        public delegate void FinalCallback(string sql, Dictionary<string, List<T>> finResDic);
+        private Dictionary<string, List<T>> finResDic;
         public SubTableQueryHelper()
         {
 
         }
 
-        public void Run(List<string> listQueryStr, Callback callback)
+        public void Run(List<string> listQueryStr, FinalCallback callback)
         {
+            finResDic = new Dictionary<string, List<T>>();
+            currentNum = 0;
             allQueryNum = listQueryStr.Count;
             foreach (var qStr in listQueryStr)
             {
@@ -37,8 +43,43 @@ namespace spc_client.Tools
                     finally
                     {
                         spcModel.Dispose();
+                        lock (finResDic)
+                        {
+                            finResDic.Add(sql, res);
+                            currentNum++;
+                        }
+                        if (currentNum >= allQueryNum)
+                        {
+                            callback(sql, finResDic);
+                        }
+                    }
+                }, qStr);
+            }
+        }
+
+        public void Run(List<string> listQueryStr, Callback callback)
+        {
+            currentNum = 0;
+            allQueryNum = listQueryStr.Count;
+            foreach (var qStr in listQueryStr)
+            {
+                MySmartThreadPool.Instance().QueueWorkItem((sql) =>
+                {
+                    SpcModel spcModel = DB.Instance();
+                    List<T> res = new List<T>();
+                    try
+                    {
+                        res = spcModel.Database.SqlQuery<T>(sql).ToList();
+                    }
+                    catch (Exception er)
+                    {
+
+                    }
+                    finally
+                    {
                         currentNum++;
-                        callback(res, currentNum == allQueryNum);
+                        callback(sql, res, currentNum == allQueryNum);
+                        spcModel.Dispose();
                     }
                 }, qStr);
             }
